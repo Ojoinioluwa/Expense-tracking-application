30489b2d9f44569b66dc2c91db389b7f99c875d1
Ojoinioluwa <105636468+Ojoinioluwa@users.noreply.github.com>
2024-07-11 12:42:40 -0700

    Update transactionRouter.js
    remove api/v1 from the route

--- backend/routes/transactionRouter.js
+++ backend/routes/transactionRouter.js
@@ -7,28 +7,28 @@ const transactionRouter = express.Router();
 
 // ? add
 transactionRouter.post(
-  "/api/v1/transactions/create",
+  "/transactions/create",
   isAuthenticated,
   transactionController.create
 );
 
 // ? filter && list the transaction by the users info or by the user alone
 transactionRouter.get(
-  "/api/v1/transactions/lists",
+  "/transactions/lists",
   isAuthenticated,
   transactionController.getFilteredTransactions
 );
 
 // ? Update
 transactionRouter.put(
-  "/api/v1/transactions/update:id",
+  "/transactions/update:id",
   isAuthenticated,
   transactionController.update
 );
 
 // ? Update
 transactionRouter.delete(
-  "/api/v1/transactions/delete:id",
+  "/transactions/delete:id",
   isAuthenticated,
   transactionController.delete
 );
